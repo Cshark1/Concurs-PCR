8b2afcd0e9e9ee74e1402aaefadadd1cc453573c
Cshark <stefan.ss811@gmail.com>
2020-04-24 18:52:12 +0300

    Addep behaivior for PowerUPs

---
+++ Concurs PCR 2020/Assets/Scripts/PowerUps.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUps : MonoBehaviour
+{
+    [SerializeField] private float _rotationSpeed = 120f;
+    [SerializeField] private byte _powerUpID;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        switch (_powerUpID)
+        {
+            
+        }
+    }
+
+    private void PowerUpRotation()
+    {
+        transform.Rotate(new Vector3(0f,1f,0f), 1f * Time.deltaTime * _rotationSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PowerUpRotation();
+    }
+}
