562231b692bd27116100253e8210a0f1921cb798
Cshark <stefan.ss811@gmail.com>
2020-04-24 11:56:06 +0300

    Camera now follows the player

--- Concurs PCR 2020/Assets/Scripts/CameraScript.cs
+++ Concurs PCR 2020/Assets/Scripts/CameraScript.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
+    [SerializeField] private GameObject _player;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void CameraFollowPlayer()
+    {
+        if (_player.transform.position.x >= 0)
+        {
+            transform.position = new Vector3(_player.transform.position.x,transform.position.y,-10f);
+            return;
+        }
+
+        transform.position = new Vector3(0f,_player.transform.position.y,-10f);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        CameraFollowPlayer();
     }
 }
